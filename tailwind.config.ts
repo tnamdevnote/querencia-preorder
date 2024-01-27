6a433837a6bf7ce0982c080991d3687a3245ef55
Taek Been Nam <taekbeen93@gmail.com>
2024-01-27 15:43:48 -0500

    font configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,33 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary-dark': '#2A254B',
+        primary: '#4E4D93',
+        'light-gray': '#F9F9F9',
+        'border-gray': '#EBE8F4',
+        'border-dark': '#CAC6DA',
+      },
+      fontFamily: {
+        headings: ['var(--font-montserrat)', 'sans'],
+        body: ['var(--font-dm_sans)', 'sans'],
+      },
+      fontSize: {
+        sm: ['0.875rem', '150%'],
+        md: ['1rem', '150%'],
+        lg: ['1.25rem', '150%'],
+        '3xl': ['2rem', '150%'],
       },
     },
   },
